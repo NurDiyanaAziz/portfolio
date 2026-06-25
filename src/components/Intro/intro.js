a65b1dca31e60e4763f5078cbd5eb3d60c082c38
NurDiyanaAziz <nurdiyanaabaziz26@gmail.com>
2026-06-25 19:41:52 +0800

    update resume typo

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,7 +3,7 @@ import './intro.css';
 import bg from '../../assets/profile5.png';
 import Github from '../../assets/github.png';
 import Linkedin from '../../assets/linkedin.png';
-import Resume from '../../assets/src/assets/RESUME-NUR_DIYANA_AB_AZIZ.pdf';
+import Resume from '../../assets/RESUME-NUR_DIYANA_AB_AZIZ.pdf';
 
 const Intro = () => {
     return (
